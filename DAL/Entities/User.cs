1ef89f4ffaa65d9bc38757c3fb169930d9fbbc24
Pavel Kazutsin <kazyten@ya.ru>
2022-02-27 11:07:49 +0300

    add User entity

---
+++ DAL/Entities/User.cs
@@ -0,0 +1,29 @@
+﻿#nullable enable
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace DAL.Entities
+{
+    public class User : IComparable
+    {
+        [Key] public string Id { get; set; }
+
+        public string FullName { get; set; }
+
+        [DataType(DataType.Text)] public DateTime Birthday { get; set; }
+
+        public ICollection<Group> Groups { get; set; }
+
+        public ICollection<Project> InProjects { get; set; }
+
+        public ICollection<Project> UserProjects { get; set; }
+
+        public ICollection<ProjectTask> Tasks { get; set; }
+
+        public int CompareTo(object? obj)
+        {
+            return Id.CompareTo((obj as User)?.Id);
+        }
+    }
+}
