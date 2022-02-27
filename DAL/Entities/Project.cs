5d8c9cc4b05ee38d33f5cd9cfadf70f39e5e5052
Pavel Kazutsin <kazyten@ya.ru>
2022-02-27 11:07:17 +0300

    add Project entity

---
+++ DAL/Entities/Project.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Project
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        
+        public ICollection<User> Participants { get; set; }
+
+        public ICollection<ProjectTask> Tasks { get; set; }
+        
+        public string ProjectOwner { get; set; }
+        
+        public string Name { get; set; }
+        
+        public DateTime ProjectStart { get; set; }
+        
+        public DateTime ProjectEnd { get; set; }
+    }
+}
