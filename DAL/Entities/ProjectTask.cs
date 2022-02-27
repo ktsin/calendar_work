3d0c93a25d812882dd4d4e5db876199882a0a98f
Pavel Kazutsin <kazyten@ya.ru>
2022-02-27 11:07:41 +0300

    add Project task entity

---
+++ DAL/Entities/ProjectTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DAL.Entities
+{
+    public class ProjectTask
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        
+        public int ProjectId { get; set; }
+        
+        public ICollection<User> Participants { get; set; }
+        
+        public string TaskName { get; set; }
+        
+        public ICollection<Tag> Tags { get; set; }
+        
+        public TaskPriority Priority { get; set; }
+        
+        public DateTime TaskStart { get; set; }
+        
+        public DateTime TaskEnd { get; set; }
+        
+    }
+}
---
+++ DAL/Entities/Tag.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Tag
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        public string OwnerId { get; set; }
+
+        public KnownColor TagColor { get; set; } = KnownColor.Silver;
+        
+        public ICollection<CalendarEvent> CalendarEvents { get; set; }
+        
+        public ICollection<ProjectTask> ProjectTasks { get; set; }
+    }
+}
