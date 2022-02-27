604e4fe63c9b3b25bb49fe8a15ed07dc285a1171
Pavel Kazutsin <kazyten@ya.ru>
2022-02-27 11:07:03 +0300

    Add group entity

---
+++ DAL/Entities/Group.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace DAL.Entities
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        
+        public string CommandOwner { get; set; }
+        
+        public string CommandName { get; set; }
+        
+        public ICollection<User> GroupParticipants { get; set; }
+    }
+}
