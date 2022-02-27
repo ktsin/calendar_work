6ecc62a226afdb9f90686b121f96a49609eeabab
Pavel Kazutsin <kazyten@ya.ru>
2022-02-27 11:08:03 +0300

    add message

---
+++ DAL/Entities/Message.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Message
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+
+        public string Sender { get; set; }
+
+        public string Recipient { get; set; }
+
+        [DataType(DataType.Text)]
+        public DateTime Sended { get; set; }
+
+        public string MessageBody { get; set; }
+    }
+}
