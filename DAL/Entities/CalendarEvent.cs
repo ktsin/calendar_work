22f6ffbc2ebd68a0a4bdc6e199e44900da02ee11
Pavel Kazutsin <kazyten@ya.ru>
2022-02-27 11:06:52 +0300

    add CalendarEvent entity

---
+++ DAL/Entities/CalendarEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DAL.Entities
+{
+    public class CalendarEvent
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public string Id { get; set; }
+
+        public string OwnerId { get; set; }
+
+        public DateTime EventDate { get; set; }
+
+        public TimeSpan Duration { get; set; }
+
+        public string Comment { get; set; }
+
+        public ICollection<Tag> Tags { get; set; }
+    }
+}
